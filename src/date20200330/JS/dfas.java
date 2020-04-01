9e35f9c06225ce14e7f49a915a1459b32e58acc6
mawei-mutou <1321053879@qq.com>
2020-04-01 23:09:59 +0800

    第三十次次提交

---
+++ src/date20200330/JS/dfas.java
@@ -0,0 +1,37 @@
+package date20200330.JS;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * created with IntelliJ IDEA
+ * author      : YaoDong
+ * date        : 2020/3/30
+ * time        : 15:23
+ * description :
+ */
+public class dfas {
+    public static void main(String[] args) {
+//        int a=6;
+//        String b= "6";
+//        String c= "a";
+////        boolean r=(a>6)&&(b<5);
+////        System.out.println(a>c);
+////        System.out.println(b == a);
+//        System.out.println(a);
+        int score=98;
+//
+//        switch (true) {//在java里面若score为整数，score/10 结果会自动处理为整数
+//            case score/10 >=6:
+//                System.out.println(score);
+//
+//                break;
+
+        List<Integer> a = new ArrayList<>();
+        a.add(1);
+        a.add(2);
+        a.forEach(s->{
+            System.out.println(s);
+        });
+        }
+}
